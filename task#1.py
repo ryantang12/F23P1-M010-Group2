01d46fc164d38eaf9d63d4c3464920b2caf073fa
Ryan Tang <107235922+ryantang12@users.noreply.github.com>
2023-09-26 08:29:45 -0400

    task#1.py

--- task#1.py
+++ task#1.py
@@ -1,2 +1,8 @@
-print("hello")
-print("hello")
+import pandas as pd
+
+wb = pd.read_excel('P1Dictionary,xlsx', dtype=str)
+bins = list(wb["bin_List"])
+chars = list(wb["char_List"])
+
+for i in range(len(bin)):
+	print(bins[i], "; ", chars[i])
