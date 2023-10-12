a955ade9932b9077eacb24c2031eeaac5716239e
Jushinc <143717263+Jushinc@users.noreply.github.com>
2023-10-12 08:48:59 -0400

    Create Task6.py

---
+++ Task6.py
@@ -0,0 +1,28 @@
+def compare_files(file1_name, file2_name="TextOutput.txt") -> bool:
+    try:
+        # Open the first file and read its contents
+        with open(file1_name, 'r') as file1:
+            content1 = file1.read()
+
+        # Open the second file and read its contents
+        with open(file2_name, 'r') as file2:
+            content2 = file2.read()
+
+        # Check if the contents of the two files are identical
+        if content1 == content2:
+            return True
+        else:
+            return False
+
+def are_strings_identical(string1, string2):
+    # Check if the lengths of the two strings are the same
+    if len(string1) != len(string2):
+        return False
+
+    # Go through the characters of both strings and compare them
+    for i in range(len(string1)):
+        if string1[i] != string2[i]:
+            return False
+
+    # If the loop completes without finding any differences, the strings are identical
+    return True
