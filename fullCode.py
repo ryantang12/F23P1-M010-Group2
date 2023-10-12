2df185f3b05ac2b7f00a262c8218abba79381bab
jbiancs14 <145680099+jbiancs14@users.noreply.github.com>
2023-10-12 12:01:16 -0400

    Update fullCode.py

--- fullCode.py
+++ fullCode.py
@@ -108,35 +108,32 @@ def txt_to_bin(file_name):
     f.close()
     print(binStr)
 
-
-
-
-
-'''
+"""
     TASK #5 - BY JACOB BIANCO
     THIS FUNCTION READS IN A TEXT FILE WITH BINARY VALUES AND
     CREATES A NEW FILE THAT CONTAINS THE CHARACTERS FOR THE
     GIVEN FILE
-'''
+"""
+
 
 def bin_to_txt(file_name='BinOutput.txt'):
-    '''
+    """
     THE FOLLOWING BLOCK OF CODE OPENS THE FILE, READS INTO THE FILE
     THEN CLOSES THE FILE
-    '''
+    """
 
-    f = open(file_name, 'r')        
-    s = f.read()                    
-    f.close()                    
-    print(s)                        
+    f = open(file_name, 'r')
+    s = f.read()
+    f.close()
+    print(s)
 
     '''
     THE FOLLOWING BLOCK OF CODE REMOVES THE DECIMAL NUMBER AND 
     PERIOD FROM THE BEGINNING OF THE STRING 
     '''
-    i = s.index(".")             
-    s = [i + 1]                 
-    print(s)                   
+    i = s.index(".")
+    s = [i + 1]
+    print(s)
 
     '''
     THE FOLLOWING BLOCK OF CODE LOOPS THROUGH A SEQUENCE OF BINARY
@@ -145,18 +142,19 @@ def bin_to_txt(file_name='BinOutput.txt'):
     '''
 
     initial = ''
-    while s != '':                          
-        binVal, s = getBFirstBin(s)  
-    initial += getChar(binVal)                  
-    
+    while s != '':
+        binval, s = getBFirstBin(s)
+    initial += getChar(binval)
+
     '''
     THE FOLLOWING BLOCK OF CODE PRINTS THE OUTPUTTED CHARACTER STRING
     INTO A TEXT FILE
     '''
-    f = open('TextOutput.txt', 'w+')            
-    f.write(initial)                            
-    f.close()                                 
-    print(initial)                              
+    f = open('TextOutput.txt', 'w+')
+    f.write(initial)
+    f.close()
+    print(initial)
+
 
 # TASK 6
 def compare_files(file1_name:str , file2_name:str ="TextOutput.txt") -> bool:
