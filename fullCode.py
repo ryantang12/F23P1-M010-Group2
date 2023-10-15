9c76d79a190147633ccea7573282ed5864fa0852
Ryan Tang <107235922+ryantang12@users.noreply.github.com>
2023-10-15 17:39:26 -0400

    Update fullCode.py

--- fullCode.py
+++ fullCode.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 15 16:14:25 2023
+
+@author: ryan_
+"""
+
 # This function will read an excel file and print out the corresponding character and it's binary conversion
 # TASK #1
 import pandas as pd
@@ -16,43 +23,44 @@ for i in range(len(bins)):
 # This function will read in a string and return the corresponding binary value for that string
 # TASK #2
 def str_to_bin(word):   
-    existing_double_char_list = ["th", "er", "on", "ss", "en", "te", "de"]
-    existing_double_int_list = [1111001, 1111010, 1111011, 1111100, 1111101, 1111110, 1111111]
-    newList = []
-    
     binary = 0
     newWord = word[0]
-    check_word = word[0] + word[1]
-        
     
+    if word[0] == " ":
+        binary = "1110101"
+        newWord = word[1:]
+        return binary, newWord
     
-    for i in range(len(bins)): # adds the character in a list to 
-        if newWord == chars[i]:
-            newList.append([bins[i]]) # list to take binary from
-            binary = bins[i]
-    newWord = word[1:]
-    
-    # if the character being converted is more than one character run this statment
-    if check_word in existing_double_char_list:
-        for i in range(len(existing_double_char_list)):
-            if check_word == existing_double_char_list[i]:
-                binary = existing_double_int_list[i]
+    if word[0:2] == "\n":
+        binary = "1110100"
         newWord = word[2:]
-    
-    print(newWord)
-    print(binary)
-    
-    return binary, newWord
+        return binary, newWord
             
-print(str_to_bin('hello'))
+    if len(word) >= 2:
+        for i in range(len(chars)):
+            if word[0:2] == chars[i]:
+                binary = bins[i]
+                newWord = word[2:]
+                return binary, newWord
+        
+    if len(word) >= 1:
+        for i in range(len(chars)):
+            if chars[i] == word[0]:
+                binary = bins[i]
+                newWord = word[1:]
+                return binary, newWord
+            
+print(str_to_bin('\n'))
 print(str_to_bin('one'))
-print(str_to_bin('ssep'))        
+print(str_to_bin('ssep'))     
+print(str_to_bin(' '))
+print(str_to_bin('"hello"')) 
 
 
 
 # TASK #3
 # This function reads in a string of binary values and returns the first binary value in the string as well as the string minus the frist binary value
-def getBFirstBin(string):
+def getBFirstBin(string: str):
     flag = string[0]
     
     if flag == "0": # this is the short binary value
@@ -76,35 +84,35 @@ def getChar(string):
 ###THE FUNCTION READS IN A TEXT FILE AND CREATES A NEW TEXT FILE CALLED "BinOutput.txt" THAT CONTAINS THE BINARY CODES FOR THE GIVEN FILE.###
 # TASK 4 By Jiayuan Zhang (MICHAEL)#
 def txt_to_bin(file_name):
-    s = ''
 #TAKE A STRING AS INPUT TO THE FUNCTION
     f = open (file_name, "r")                    #OPEN THE FILE
-    s1 = f.read()                                #READ IN THE VALUES
+    s = f.read()                                #READ IN THE VALUES
     f.close()                     
                #CLOSE THE FILE
 
-    print(s1)
+    print(s)
 #FUNCTION FIND ALL THE BINARY CODES FOR THE GIVEN TEXT FILE
     binStr = ''
     numBits = 0
     binVal = 0
-    BinStr = ''
     
     while (s != ''):
-        print(binStr, '\n', s1) 
-        binVal, s = str_to_bin(s1)
-        binStr = BinStr + binVal                  #DETERMINE THE NUMBER OF BITS NEEDED TO STORE THE TEXT FILE
-        print(binStr)
-        numBits = len(binStr)
-        binStr = str(numBits) + "." + binStr        # "." is use to separate them
-
+        # print(binStr, '\n', s1) 
+        binVal, s = str_to_bin(s)
+        binStr = binStr + binVal                  #DETERMINE THE NUMBER OF BITS NEEDED TO STORE THE TEXT FILE
+        
+    print(binStr)
+    numBits = len(binStr)
+    binStr = str(numBits) + "." + binStr        # "." is use to separate them
+        
+        
 #WRITE THE BINARY VALUES TO A FILE NAMED "BinOutput.txt" 
-    f = open ("BinOutput.txt", "w+'")
+    f = open ("BinOutput.txt", "w+")
     f.write(binStr)
     f.close()
     print(binStr)
 
-print(txt_to_bin("Hill.txt"))
+print(txt_to_bin('Hill.txt'))
 
 """
     TASK #5 - BY JACOB BIANCO
@@ -156,33 +164,37 @@ def bin_to_txt(file_name='BinOutput.txt'):
 print(bin_to_txt('BinOutput.txt'))
 
 
-# def compare_files(file1_name:str , file2_name:str ="TextOutput.txt") -> bool:
-#     try:
-#         # Open the first file and read its contents
-#         with open(file1_name, 'r') as file1:
-#             content1 = file1.read()
+def compare_files(file1_name: str, file2_name: str = "TextOutput.txt") -> bool:
+    try:
+        # Open the first file and read its contents
+        with open(file1_name, 'r') as file1:
+            content1 = file1.read()
 
-#         # Open the second file and read its contents
-#         with open(file2_name, 'r') as file2:
-#             content2 = file2.read()
+        # Open the second file and read its contents
+        with open(file2_name, 'r') as file2:
+            content2 = file2.read()
 
-#         # Check if the contents of the two files are identical
-#         if content1 == content2:
-#             return True
-#         else:
-#             return False
+        # Check if the contents of the two files are identical
+        if content1 == content2:
+            return True
+        else:
+            return False
+
+    except FileNotFoundError:
+        print("One or both files not found.")
+        return False
 
 
+def are_strings_identical(string1: str, string2: str) -> bool:
+    # Check if the lengths of the two strings are the same
+    if len(string1) != len(string2):
+        return False
 
-# def are_strings_identical(string1:str, string2:str) -> bool:
-#     # Check if the lengths of the two strings are the same
-#     if len(string1) != len(string2):
-#         return False
+    # Go through the characters of both strings and compare them
+    for i in range(len(string1)):
+        if string1[i] != string2[i]:
+            return False
 
-#     # Go through the characters of both strings and compare them
-#     for i in range(len(string1)):
-#         if string1[i] != string2[i]:
-#             return False
+    # If the loop completes without finding any differences, the strings are identical
+    return True
 
-#     # If the loop completes without finding any differences, the strings are identical
-#     return True
